789f7fe59456dfe4214185f5518caf7480367040
krnlv <irazor191119@gmail.com>
2015-10-18 17:39:02 +0700

    Create save_event.php

---
+++ save_event.php
@@ -0,0 +1,33 @@
+<head><meta http-equiv=Content-Type content="text/html; charset=utf-8">
+    <link href="css/font-awesome.css" rel="stylesheet" media="all"></head>
+<?php
+?>
+<?php
+include_once 'connect.php';
+
+$cur_date = date("Y-m-d");
+
+$sql = "INSERT INTO event_dtl ("
+        . "txtdetail,txtdestination,"
+        . "start_date,end_date,"
+        . "date_added,status)"
+        . "VALUES ("
+        . "'" . $_POST['txtdetail'] . "',"
+        . "'" . $_POST['txtdestination'] . "',"
+        . "'" . $_POST['start_date'] . "',"
+        . "'" . $_POST['end_date'] . "',"
+        . "'" . $cur_date . "',"
+		. "'" . $_POST['status'] . "'"
+        . ")";
+$qry = mysql_query($sql);
+
+if ($qry) {
+    echo '<center>INSERT Complete</center>';
+    echo "<center><i class='fa fa-spinner fa-spin fa-5x'></i></center>";
+	echo "<meta http-equiv='refresh' content='1;URL=calendar.php'>";
+} else {
+    echo 'Error Insert';
+	echo $sql;
+}
+
+?>
