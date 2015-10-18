51fcd89f5ec8c45b5b09432117b6d8bd8b709e85
krnlv <irazor191119@gmail.com>
2015-10-18 17:37:54 +0700

    Update calendar_event_detail.php

--- calendar_event_detail.php
+++ calendar_event_detail.php
@@ -2,7 +2,7 @@
     <head>
         <title>B Event on Calendar</title>
         <meta http-equiv=Content-Type content="text/html; charset=utf-8">
-        <link href="css/bootstrap.css" rel="stylesheet" media="all">
+        <link href="https://bootswatch.com/flatly/bootstrap.css" rel="stylesheet" media="all">
         <link href="css/font-awesome.css" rel="stylesheet" media="all">
 
     </head>
